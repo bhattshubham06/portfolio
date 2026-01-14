b31ee6c29748e7c36a79a939daae2917328bb3f2
DKDVE <dhruvkalpeshdave@gmail.com>
2026-01-14 17:40:33 +0000

    Resolved Typed routes error

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,6 @@ const nextConfig = {
   },
   assetPrefix: isProd ? `/${repo}` : undefined,
   basePath: isProd ? `/${repo}` : undefined,
-  experimental: {
-    typedRoutes: true
-  }
 };
 
 export default nextConfig;

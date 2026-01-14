e3d77b86218919942206bb7d240b9cc7ce1f6db9
DKDVE <dhruvkalpeshdave@gmail.com>
2026-01-14 17:21:06 +0000

    chore: clean build artifacts

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/mnt/e/Portfolio/Shubham_portfolio/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/eaead17c7dbfcd5d-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
